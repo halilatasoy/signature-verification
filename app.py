40740ed3b5debf457b3f81e31bd0e2235086eb11
halil <halilatasoy@Halils-MacBook-Air-4.local>
2025-02-19 12:34:27 +0300

    Flask yapılandırması güncellendi

--- app.py
+++ app.py
@@ -11,8 +11,6 @@ def hello():
 def signature():
     return 'İmza doğrulama servisi yakında burada olacak!'
 
-if __name__ == '__main__':
-    # OpenShift'in PORT environment variable'ını kullan
-    port = int(os.environ.get('PORT', 8080))
-    # Host'u 0.0.0.0 olarak ayarla
-    app.run(host='0.0.0.0', port=port, debug=False) 
+# Ana blok dışında çalıştır
+port = int(os.environ.get('PORT', 8080))
+app.run(host='0.0.0.0', port=port) 
